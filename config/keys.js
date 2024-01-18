003f27ad110194d8d5b43c485156baf126420a6e
namasyakiet <156899831+namasyakiet@users.noreply.github.com>
2024-01-18 15:45:15 +0530

    version __01

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    clientID:"925228375861-nu60ku8etifgeb53vm5cviofjs61kmmj.apps.googleusercontent.com",
+    clientSecret: "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",
+    // mongoURI:"mongodb+srv://harshityadav78291:buZd1yZNXqerbf8G@cluster0.k03fncj.mongodb.net/?retryWrites=true&w=majority",
+    //     sessionSecret:"hisisasecretkey"
+};
